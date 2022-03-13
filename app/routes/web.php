60313edaa389cc09895299175f6caba1cff095e6
XshiranonX <antuki0502@gmail.com>
2022-03-13 13:13:24 +0900

    fix

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/book', [BookController::class, 'index']);
+Route::get('/wel', function () {
     return view('welcome');
 });
+
